1bc2f16c3071169d31fcbbe17705c3f1599fd63f
Tobias Seidel <tobiasseidel@mail.de>
2021-03-09 23:14:32 +0100

    Updated setup build

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Simple module for downloading financial statements and estimates from financials
 Install using pip:
 
 ```
-pip install -i https://test.pypi.org/simple/ mfinacials
+pip install --extra-index-url https://test.pypi.org/simple/ mfinancials
 ```
 or install from source:
 
--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools.setup()
+setup(
+    packages=find_packages(exclude=['contrib', 'docs', 'tests', 'examples']),
+    install_requires=[
+        'beautifulsoup4>=4.9.1',
+        'numpy>=1.18.5',
+        'requests>=2.24.0',
+        'yfinance>=0.1.54',
+        'pandas>=1.1.2',
+        'lxml>=4.5.2'
+    ]
+)
